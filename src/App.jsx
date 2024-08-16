25f70f89f2fd512a1400b17381b73de22686fe6f
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-08-16 23:55:00 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Promotion from "./components/Promotion"
+import ProductSlider from "./components/ProductSlider"
+import Footer from "./components/Footer"
+import './App.css'
 
 function App() {
   
@@ -8,6 +12,9 @@ function App() {
     <div>
       <Header/>
       <Hero/>
+      <Promotion/>
+      <ProductSlider/>
+      <Footer/>
     </div>
     </>
   )
