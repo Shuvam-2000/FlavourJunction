01a6c784547a4b46d0131f613630aa196feba0fc
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-08-17 11:00:00 +0530

    home section containing the home page components

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import ProductSlider from "./ProductSlider"
+import Promotion from './Promotion'
+import Hero from "./Hero"
+
+const Home = () => {
+  return (
+    <div>
+        <Hero/>
+        <Promotion/>
+        <ProductSlider/>
+    </div>
+  )
+}
+
+export default Home
