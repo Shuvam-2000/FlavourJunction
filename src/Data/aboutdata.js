0ce364a78f670c86fafdfb74dc6f648bbce6b5cb
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-08-18 17:39:51 +0530

    dataset for about us slider

---
+++ src/Data/aboutdata.js
@@ -0,0 +1,43 @@
+export const prodcutData = [
+    {
+        id: 1,
+        title: "TECHNOLOGY",
+        url: "https://careers.swiggy.com/assets/img/icons/ic_1.png"
+    },
+    {
+        id: 2,
+        title: "BUSINESS",
+        url: "https://careers.swiggy.com/assets/img/icons/ic_2.png"
+    },
+    {
+        id: 3,
+        title: "CLOUD KITCHEN",
+        url: "https://careers.swiggy.com/assets/img/icons/ic_3.png"
+    },
+    {
+        id: 4,
+        title: "DELIVERY",
+        url: "https://careers.swiggy.com/assets/img/icons/ic_3.png" 
+    },
+    {
+        id: 4,
+        title: "CUSTOMER CARE",
+        url: "https://careers.swiggy.com/assets/img/icons/ic_4.png"
+    },
+    {
+        id: 5,
+        title: "CAMPUS",
+        url: "https://careers.swiggy.com/assets/img/icons/ic_5.png"
+    },
+    {
+        id: 6,
+        title: "CORPORATE SUPPORT",
+        url: "https://careers.swiggy.com/assets/img/icons/ic_6.png"
+        
+    },
+    {
+        id: 7,
+        title: "RETAILERS SUPPORT",
+        url: "https://careers.swiggy.com/assets/img/icons/ic_6.png"
+    }
+];
