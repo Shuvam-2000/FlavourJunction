7114c407ac915eaa46c33c1841121c3f98239b16
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-08-19 23:40:14 +0530

    add to cart counter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Header from "./components/Header"
-import Home from './components/Home'
-import AboutUS from './components/AboutUS'
-import ExploreMore from './components/ExploreMore'
-import SignIn from './components/SignIn'
-import Footer from "./components/Footer"
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Header from "./components/Header";
+import Home from './components/Home';
+import AboutUS from './components/AboutUS';
+import ExploreMore from './components/ExploreMore';
+import SignIn from './components/SignIn';
+import Footer from "./components/Footer";
+import './App.css';
 
 function App() {
-  
+  const [count, setCount] = useState(0);
+
+  const handleAdd = () => {
+    setCount(count + 1)
+  }
+
   return (
-    <>
-    <div>
     <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/aboutus' element={<AboutUS/>}/>
-          <Route path='/exploremore' element={<ExploreMore/>}/>
-          <Route path='/signin' element={<SignIn/>}/>
-        </Routes>
-        <Footer/>
-        </BrowserRouter>
-    </div>
-    </>
-  )
+      <Header count={count} />
+      <Routes>
+        <Route path='/' element={<Home handleAdd={handleAdd}/>} />
+        <Route path='/aboutus' element={<AboutUS />} />
+        <Route path='/exploremore' element={<ExploreMore />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
 
