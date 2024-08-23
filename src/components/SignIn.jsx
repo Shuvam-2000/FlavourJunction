79031016d9cd635d7c0d83ebb3feda7eed58cc20
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-08-23 16:45:08 +0530

    sign in section completed

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -21,7 +21,6 @@ const SigninPage = () => {
                   type="email"
                   autoComplete="email"
                   required
-                  value="email"
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none"/>
               </div>
             </div>
