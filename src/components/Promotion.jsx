347553d5ee695e840f12c65704b2bb8da16cfa63
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-10-02 16:43:46 +0530

    styling errors fixed

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -2,11 +2,11 @@ const Delivery = () => {
     return (
       <>
         <div className="w-full py-8 px-4">
-          <h3 className="text-[#fabb02] font-serif font-bold sm:text-4xl  text-2xl text-center pb-10">Get Your Delicious & <span className="text-black">Healthy Meal</span></h3>
+          <h3 className="text-[#fabb02] font-mono sm:text-3xl text-xl text-center pb-10">Get Your Delicious & <span className="text-black">Healthy Meal</span></h3>
           <div className="flex justify-center items-center flex-col mb-12">
-            <img src="https://media.designrush.com/inspiration_images/256686/conversions/Food_Delivery_3_c6e96ba529ad-mobile.jpg" alt="Food Delivery" />
-            <h1 className="font-bold font-serif sm:text-2xl text-2xl text-center mx-10 my-4"><span className="text-[#fabb02]">Download The App</span> & Get Your Favorite Food Quickly</h1>
-            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-3 px-4 rounded">Download Now</button>
+            <img className="w-[200px] sm:w-[400px]" src="https://media.designrush.com/inspiration_images/256686/conversions/Food_Delivery_3_c6e96ba529ad-mobile.jpg" alt="Food Delivery" />
+            <h1 className="font-mono sm:text-2xl text-sm text-center mx-10 my-4"><span className="text-[#fabb02]">Download The App</span> & Get Your Favorite Food Quickly</h1>
+            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold sm:py-3 sm:px-4 py-3 px-2 text-xs sm:text-sm rounded">Download Now</button>
             <div className="mt-4 border-black"></div>
           </div>
         </div>
