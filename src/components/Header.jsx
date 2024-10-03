c056f5c6924825ebd82dfdffe334a4224837f803
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-10-03 16:02:23 +0530

    slightly modified the routing and added the cart item count funnctionality to the cart icon in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import { MdClose } from "react-icons/md";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import cart_icon from "../assets/cart_icon.png"
 import menu_icon from "../assets/menu_icon.png"
 
 const Header = () => {
 
-  // side navbar 
+  // state for side navbar in mobile view 
   const[showSideBar, setSideBar] = useState(false)
+
+  // add to cart functionality
+  const cartCount = useSelector((state) => state.cart.length);
 
   return (
     <div className='flex justify-between py-[12px] px-[20px] bg-[#f1f3f5] border-b font-mono cursor-pointer'>
       <div>
-        <h1 className='sm:text-3xl text-2xl font-bold'>
+        <h1 className='sm:text-3xl text-xl font-bold'>
         Flavour
         <span className='text-[#fabb02]'>
         Junction</span></h1>
@@ -24,12 +28,12 @@ const Header = () => {
             <li className="hover:text-[#fabb02]">Home</li>
             <hr className="w-3/2 border-none h-[1.5px] bg-[#fabb02] hidden"/>
           </NavLink>
-          <NavLink to='/aboutus'>
-            <li className="hover:text-[#fabb02]">About Us</li>
+          <NavLink to='/exploremore'>
+            <li className="hover:text-[#fabb02]">Our Menu</li>
             <hr className="w-3/2 border-none h-[1.5px] bg-[#fabb02] hidden"/>
           </NavLink>
-          <NavLink to='/exploremore'>
-            <li className="hover:text-[#fabb02]">Explore Menu</li>
+          <NavLink to='/aboutus'>
+            <li className="hover:text-[#fabb02]">About Us</li>
             <hr className="w-3/2 border-none h-[1.5px] bg-[#fabb02] hidden"/>
           </NavLink>
           <NavLink to='/signin'>
@@ -45,19 +49,15 @@ const Header = () => {
           className="absolute top-2 left-4 cursor-pointer" 
           onClick={() => setSideBar(!showSideBar)}/>
 
-          {/* <FaCartShopping size="30px" 
-          className="absolute bottom-40 top-68 mx-8 my-18 hover:text-[#fabb02]" 
-          onClick={() => setSideBar(!showSideBar)}/> */}
-
           <ul className='flex text-[15px] font-bold py-[6px] pt-[12vw] flex-col space-y-20'>
             <NavLink to='/'>
               <li className="hover:text-[#fabb02]" onClick={() => setSideBar(!showSideBar)}>Home</li>
             </NavLink>
+            <NavLink to='/exploremore'>
+              <li className="hover:text-[#fabb02]" onClick={() => setSideBar(!showSideBar)}>Our Menu</li>
+            </NavLink>
             <NavLink to='/aboutus'>
               <li className="hover:text-[#fabb02]" onClick={() => setSideBar(!showSideBar)}>About Us</li>
-            </NavLink>
-            <NavLink to='/exploremore'>
-              <li className="hover:text-[#fabb02]" onClick={() => setSideBar(!showSideBar)}>Explore More</li>
             </NavLink>
             <NavLink to='/signin'>
               <li className="hover:text-[#fabb02]" onClick={() => setSideBar(!showSideBar)}>Sign In</li>
@@ -71,10 +71,10 @@ const Header = () => {
       <div className="flex items-center gap-4">
           {/* Cart Icon */}
           <div className="group relative">
-            <NavLink to='/cart' className="relative">
+            <NavLink to='/cartpage' className="relative">
               <img src={cart_icon} alt="cart-icon" className="w-4 sm:w-6" />
               <p className="absolute sm:right-[-5px] right-[-6px] sm:bottom-[-5px] bottom-[-7px] w-4 text-center leading-4 bg-[#fabb02] text-white aspect-square rounded-full text-[8px]">
-                0
+                {cartCount}
               </p>
             </NavLink>
           </div>
