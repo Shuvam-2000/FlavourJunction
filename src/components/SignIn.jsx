fbc0c8557d8fb6b4ed7c6e7f6c09878823b124a8
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-10-04 16:28:33 +0530

    routing added for the Sign Up page

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,18 +1,20 @@
+import { useNavigate } from "react-router-dom";
 
 const SigninPage = () => {
 
+  const navigate = useNavigate()
+
   return (
-    <div className= " flex flex-col justify-center w-[90%] mx-auto mb-[40px] font-serif">
+    <div className= " flex flex-col justify-center w-[90%] mx-auto mb-[40px] font-mono">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Welcome Back!</h2>
+        <h2 className="mt-6 text-center text-2xl sm:text-3xl text-gray-900">Welcome Back!</h2>
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md w-2vw">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg rounded-lg sm:px-10 border border-black">
           <form className="space-y-6 mb-14 mt-10 sm:mb-16">
-            <div>
               <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email address
+                Email Address
               </label>
               <div className="mt-1">
                 <input
@@ -21,8 +23,6 @@ const SigninPage = () => {
                   type="email"
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none"/>
               </div>
-            </div>
-
             <div>
               <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                 Password
@@ -32,7 +32,7 @@ const SigninPage = () => {
                   id="password"
                   name="password"
                   type="password"
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none"/>
+                  className="appearance-none block w-full mt-4 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none"/>
               </div>
             </div>
 
@@ -54,9 +54,9 @@ const SigninPage = () => {
           </form>
           <div className="mt-6 text-center">
             <p className="text-sm text-gray-600">Don't have an account yet?</p>
-            <a href="#" className="font-medium text-yellow-500 hover:text-yellow-600">
+            <p onClick={() => navigate('/signup')} className="font-medium text-yellow-500 hover:text-yellow-600 cursor-pointer">
               Sign up
-            </a>
+            </p>
           </div>
         </div>
       </div>
